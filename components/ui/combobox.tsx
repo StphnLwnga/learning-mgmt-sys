93d80021e06e82646c757b9151984ddb0e0f8aee
StphnLwnga <boywilder99@gmail.com>
2024-01-08 20:44:28 +0200

    added combobox for course category selection

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -18,15 +18,18 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 
-
 interface ComboboxProps {
   options: { label: string; value: string }[];
   value?: string;
   onChange: (value: string) => void;
-}
+};
 
-export const Combobox = ({ options, value, onChange }: ComboboxProps): JSX.Element => {
-  const [open, setOpen] = React.useState(false);
+export const Combobox = ({
+  options,
+  value,
+  onChange
+}: ComboboxProps) => {
+  const [open, setOpen] = React.useState(false)
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -35,7 +38,7 @@ export const Combobox = ({ options, value, onChange }: ComboboxProps): JSX.Eleme
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-full justify-between"
         >
           {value
             ? options.find((option) => option.value === value)?.label
@@ -43,18 +46,17 @@ export const Combobox = ({ options, value, onChange }: ComboboxProps): JSX.Eleme
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-full p-0 h-60">
         <Command>
           <CommandInput placeholder="Search option..." />
-          <CommandEmpty>No options found.</CommandEmpty>
+          <CommandEmpty>No option found.</CommandEmpty>
           <CommandGroup>
             {options.map((option) => (
               <CommandItem
                 key={option.value}
-                value={option.value}
                 onSelect={() => {
                   onChange(option.value === value ? "" : option.value);
-                  setOpen(false);
+                  setOpen(false)
                 }}
               >
                 <Check
