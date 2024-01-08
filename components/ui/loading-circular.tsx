ddea8f37b03e8ee12e79e3a96575d036b51a6889
StphnLwnga <boywilder99@gmail.com>
2024-01-08 20:33:43 +0200

    added mui progress component

---
+++ components/ui/loading-circular.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import { SxProps, Theme } from '@mui/material';
+import { cn } from '@/lib/utils';
+
+interface LoadingCircularProps {
+  sx?: SxProps<Theme>;
+}
+
+const LoadingCircular = ({ sx }: LoadingCircularProps): JSX.Element => {
+  return (
+    <Box sx={{display: 'flex', ...sx}}>
+      <CircularProgress className={cn('text-sky-300')} />
+    </Box>
+  );
+}
+
+export default LoadingCircular;
