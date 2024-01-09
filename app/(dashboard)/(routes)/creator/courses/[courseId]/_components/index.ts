f20af512381b55e61f46d707bb718d645dcfb0b5
StphnLwnga <boywilder99@gmail.com>
2024-01-09 23:59:43 +0200

    adding index files

---
+++ app/(dashboard)/(routes)/creator/courses/[courseId]/_components/index.ts
@@ -0,0 +1,10 @@
+import TitleForm from "./title-form";
+import DescriptionForm from "./description-form";
+import ImageForm from "./image-form";
+import CategoryForm from "./category-form";
+import PriceForm from "./price-form";
+import AttachmentForm from "./attachment-form";
+
+export {
+  AttachmentForm, CategoryForm, DescriptionForm, ImageForm, PriceForm, TitleForm,
+}
