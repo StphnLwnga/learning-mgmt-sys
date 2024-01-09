26a139ec9f45d16e9b5ba93230b8d6962bc78ec6
StphnLwnga <boywilder99@gmail.com>
2024-01-09 23:50:08 +0200

    added purchase & stripe customer models & relations to schema

---
+++ prisma/migrations/20240109214923_added_stripe_customer_purchase_models_and_relations/migration.sql
@@ -0,0 +1,30 @@
+-- AlterTable
+ALTER TABLE `attachment` ALTER COLUMN `updatedAt` DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE `category` ALTER COLUMN `updatedAt` DROP DEFAULT;
+
+-- CreateTable
+CREATE TABLE `Purchase` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `courseId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Purchase_courseId_idx`(`courseId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `StripeCustomer` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `stripeCustomerId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `StripeCustomer_userId_key`(`userId`),
+    UNIQUE INDEX `StripeCustomer_stripeCustomerId_key`(`stripeCustomerId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
