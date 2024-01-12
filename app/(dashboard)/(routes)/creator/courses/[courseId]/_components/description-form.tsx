e19e5b9dc4adddedf2ad7fcf0b7d1291a79eadb9
StphnLwnga <boywilder99@gmail.com>
2024-01-12 18:02:13 +0200

    minor changes to course page components

--- app/(dashboard)/(routes)/creator/courses/[courseId]/_components/description-form.tsx
+++ app/(dashboard)/(routes)/creator/courses/[courseId]/_components/description-form.tsx
@@ -81,7 +81,7 @@ const DescriptionForm = ({ initialData, courseId, userId }: DescriptionFormProps
       toast({
         title: 'Success',
         description: "Course description successfully updated!",
-        className: "bg-emerald-700 border-0 border-slate-200",
+        className: `${isDarkTheme ? 'bg-emerald-500' : 'bg-emerald-500 text-slate-100'} border-0 border-slate-200`,
       });
       toggleEdit();
       router.refresh();
--- app/(dashboard)/(routes)/creator/create/page.tsx
+++ app/(dashboard)/(routes)/creator/create/page.tsx
@@ -85,7 +85,7 @@ const CreatePage = () => {
         title: 'Success',
         description: "Course successfully drafted!",
         onClick: () => resetForm(),
-        className: "bg-emerald-700 border-0 border-slate-200",
+        className: `${isDarkTheme ? 'bg-emerald-500' : 'bg-emerald-500 text-slate-100'} border-0 border-slate-200`,
       });
     } catch (e) {
       toast({
