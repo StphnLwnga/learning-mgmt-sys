d9c3015cfe4459416dc57c3f01019399d15995a6
StphnLwnga <boywilder99@gmail.com>
2024-01-16 13:42:28 +0200

    handled video upload and Mux in edit chapter route

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,13 @@
+import Mux from "@mux/mux-node";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+
+
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKEN_SECRET!,
+);
 
 /**
  * Updates a chapter in the database.
@@ -30,7 +37,31 @@ export async function PATCH(req: Request, { params }: { params: { courseId: stri
       data: { ...values, },
     });
 
-    // TODO: Handle video upload
+    if (values.videoUrl) {
+      // Delete existing video if it exists from Mux and the associated data from the database
+      const existingMuxData = await db.muxData.findFirst({ where: { chapterId, }, });
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId);
+        await db.muxData.delete({ where: { chapterId, }, });
+        // await db.muxData.delete({ where: { id: existingMuxData.id } });
+      }
+
+      // Add video data to Mux
+      const asset = await Video.Assets.create({
+        input: values.videoUrl,
+        playback_policy: "public",
+        test: false,
+      });
+
+      // Add Mux data to database
+      await db.muxData.create({
+        data: {
+          chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0]?.id,
+        }
+      });
+    }
 
     return NextResponse.json(chapter);
   } catch (error) {
