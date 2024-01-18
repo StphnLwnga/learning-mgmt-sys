daa280f5019c839d34d2fa161c52a109ed58c0d8
StphnLwnga <boywilder99@gmail.com>
2024-01-18 15:26:51 -0800

    added delete course API route

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,13 +1,20 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import Mux from "@mux/mux-node";
 import { NextResponse } from "next/server";
+
+
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKEN_SECRET!,
+);
 
 /**
  * Updates a course in the database.
  *
  * @param {Request} req - the request object
  * @param {Object} params - an object containing the parameters
- *   @param {string} params.courseId - the ID of the course to update
+ * @param {string} params.courseId - the ID of the course to update
  * @return {Promise<NextResponse>} a Promise that resolves to a NextResponse object
  */
 export async function PATCH(req: Request, { params }: { params: { courseId: string } }): Promise<NextResponse> {
@@ -29,3 +36,51 @@ export async function PATCH(req: Request, { params }: { params: { courseId: stri
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
+
+/**
+ * Deletes a course and its associated chapters and Mux data.
+ *
+ * @param {Request} req - The request object.
+ * @param {Object} params - The parameters object.
+ * @param {string} params.courseId - The ID of the course to delete.
+ * @return {Promise<NextResponse>} A Promise that resolves to a NextResponse object.
+ */
+export async function DELETE(req: Request, { params }: { params: { courseId: string } }): Promise<NextResponse> {
+  try {
+    const { userId } = auth();
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const { courseId } = params;
+
+    // get associated chapters & muxData
+    const course = await db.course.findUnique({
+      where: { id: courseId, userId },
+      include: {
+        chapters: {
+          include: {
+            muxData: true,
+          },
+        },
+      },
+    });
+    if (!course) return new NextResponse("Unauthorized", { status: 401 });
+
+    // Delete associated Mux data / assets
+    for (const chapter of course.chapters) {
+      if (chapter.muxData) {
+        await Video.Assets.del(chapter.muxData.assetId);
+        await db.muxData.delete({ where: { id: chapter.muxData.id } });
+      }
+    }
+
+    // Delete course
+    const deletedCourse = await db.course.delete({
+      where: { id: courseId, userId },
+    });
+
+    return NextResponse.json(deletedCourse);
+  } catch (error) {
+    console.log("[COURSEIDAPI]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
