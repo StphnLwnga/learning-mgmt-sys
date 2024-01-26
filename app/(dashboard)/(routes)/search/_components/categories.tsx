a996a132d9545c00b7721b2cc73794c0a57b77a0
StphnLwnga <boywilder99@gmail.com>
2024-01-26 05:05:42 -0800

    refactored categories component

--- app/(dashboard)/(routes)/search/_components/categories.tsx
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -7,27 +7,29 @@ import { Category } from "@prisma/client";
 import CategoryItem from "./category-item";
 import { categoryIconMap } from "./category-icon-map";
 
-interface CategoriesProps {
-  items: Category[];
-}
 
-const Categories = ({ items }: CategoriesProps): JSX.Element => {
+/**
+ * Renders a list of category items based on the provided items array.
+ *
+ * @param {Array<Category>} items - The array of category items to be rendered.
+ * @return {JSX.Element} The JSX element representing the rendered category items.
+ */
+const Categories = ({ items }: {items: Category[]}): JSX.Element => {
   const { theme } = useTheme();
 
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
   useEffect(() => {
     setIsDarkTheme(theme === "dark" ?? false);
-  }, [theme]);
+  }, [ theme]);
 
   return (
     <div className="flex items-center gap-x-4 gap-y-3 pb-2 flex-wrap justify-start">
-      {items.map(item => (
+      {items.map((item) => (
         <CategoryItem
-          key={item.id}
-          label={item.name}
-          icon={categoryIconMap[item.name]}
-          value={item.id}
+          key={item?.id}
+          icon={categoryIconMap[item?.name]}
+          item={item}
           isDarkTheme={isDarkTheme}
         />
       ))}
