029207d27b8cefc6a088f995f17392eed0862683
StphnLwnga <boywilder99@gmail.com>
2024-01-26 04:57:00 -0800

    fixed types in category hook

--- lib/hooks/useCategoryData.tsx
+++ lib/hooks/useCategoryData.tsx
@@ -1,25 +1,29 @@
 import { redirect } from 'next/navigation';
 import { useState, useEffect } from "react";
 import axios from 'axios';
-import { Category } from "@prisma/client";
+import { Category, Course } from "@prisma/client";
 
 import { useToast } from "@/components/ui/use-toast";
 import { ToastAction } from "@/components/ui/toast";
+
+export interface CategoriesProps {
+  categories: Category & { courses: Course[] }[];
+}
 
 /**
  * Retrieves category data from the API and returns the categories.
  *
  * @return {{ categories: Category[] }} The categories retrieved from the API
  */
-export function useCategoryData(): { categories: Category[] } {
+export function useCategoryData(): { categories: Category[]; }{
   const { toast } = useToast();
 
-  const [categories, setCategories] = useState<Category[] | []>([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     async function fetchData(): Promise<void> {
       try {
-        const { data }: { data: Category[] } = await axios.get(`/api/categories`);
+        const { data } = await axios.get(`/api/categories`);
         setCategories(data);
       } catch (error) {
         console.error("Error fetching categories data:", error);
