00a64c758759228498b918fab1e7a1192fecd948
StphnLwnga <boywilder99@gmail.com>
2024-01-26 04:58:43 -0800

    fixed props and types; added course count badge

--- app/(dashboard)/(routes)/search/_components/category-item.tsx
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -1,16 +1,17 @@
+import { useEffect, useState } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import qs from "query-string";
 import { IconType } from "react-icons";
 import Badge, { BadgeProps } from '@mui/material/Badge';
 import { styled } from '@mui/material/styles';
+import { Category, Course } from "@prisma/client";
 
 import { cn } from "@/lib/utils";
 
 interface CategoryItemProps {
-  label: string;
-  value?: string;
-  icon?: IconType;
+  item: Category;
   isDarkTheme: boolean;
+  icon?: IconType;
 }
 
 const CategoryBadge = styled(Badge)<BadgeProps>(() => ({
@@ -26,22 +27,33 @@ const CategoryBadge = styled(Badge)<BadgeProps>(() => ({
 }));
 
 /**
- * Renders a category item with label, value, and optional icon.
+ * A function that renders a category item component based on the given item, theme, and icon. 
  *
- * @param {string} label - The label of the category item.
- * @param {string} value - The value of the category item.
- * @param {React.ElementType} Icon - The optional icon component.
- * @return {JSX.Element} The rendered category item.
+ * @param {CategoryItemProps} item - the category item to be rendered
+ * @param {boolean} isDarkTheme - flag indicating whether the dark theme is enabled
+ * @param {Icon} icon - the icon component to be rendered
+ * @return {JSX.Element} the rendered category item component
  */
-const CategoryItem = ({ label, value, isDarkTheme, icon: Icon }: CategoryItemProps): JSX.Element => {
+const CategoryItem = ({ item, isDarkTheme, icon: Icon }: CategoryItemProps): JSX.Element => {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
 
+  const [label, value] = [item?.name, item?.id];
+
   const currentCategoryId = searchParams.get("categoryId");
+
+  // Preserve a user's last search term
   const currentTitle = searchParams.get("title");
 
   const isSelected = currentCategoryId === value;
+
+  const [numCourses, setNumCourses] = useState(0);
+
+  useEffect(() => {
+    const courses = Object.values(item)[Object.values(item).length - 1] as unknown as Array<Course>;
+    setNumCourses(courses.length);
+  }, [item]);
 
   /**
    * A function that handles the click event and constructs a URL based on the current pathname, 
@@ -58,13 +70,13 @@ const CategoryItem = ({ label, value, isDarkTheme, icon: Icon }: CategoryItemPro
         categoryId: isSelected ? null : value,
       },
     }, { skipNull: true, skipEmptyString: true, });
-    
+
     router.push(url);
   }
 
   return (
     <div className="">
-      <CategoryBadge badgeContent={0} color="primary" >
+      <CategoryBadge badgeContent={numCourses} color="primary" >
         <button type="button"
           onClick={onClick}
           className={cn(
