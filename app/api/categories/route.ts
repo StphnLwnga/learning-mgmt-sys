e8fc259f4506953d27b602219ae284e3c997dd9c
StphnLwnga <boywilder99@gmail.com>
2024-01-26 16:19:33 -0800

    improved course category sorting; added doc string

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,12 +1,22 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+/**
+ * Asynchronous function to handle GET requests.
+ *
+ * @param {Request} req - The request object
+ * @return {Promise<NextResponse>} A promise that resolves to the NextResponse
+ */
 export async function GET(req: Request): Promise<NextResponse> {
   try {
     const categories = await db.category.findMany({
-      orderBy: { name: 'asc', },
       include: { courses: true, },
+      orderBy: [
+        { courses: { _count: 'desc', }, },
+        { name: 'asc', },
+      ],
     });
+    
     return NextResponse.json(categories);
   } catch (error) {
     console.log("[CATEGORIES]", error);
