787f12449705e43502cf3d54e6b19bab511ba08b
StphnLwnga <boywilder99@gmail.com>
2024-01-26 16:16:47 -0800

    horizontal category scroll - in progress

--- app/(dashboard)/(routes)/search/_components/categories.tsx
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -6,6 +6,8 @@ import { Category } from "@prisma/client";
 
 import CategoryItem from "./category-item";
 import { categoryIconMap } from "./category-icon-map";
+import { Button } from "@/components/ui/button";
+import { FaCircleArrowRight } from "react-icons/fa6";
 
 
 /**
@@ -14,17 +16,23 @@ import { categoryIconMap } from "./category-icon-map";
  * @param {Array<Category>} items - The array of category items to be rendered.
  * @return {JSX.Element} The JSX element representing the rendered category items.
  */
-const Categories = ({ items }: {items: Category[]}): JSX.Element => {
+const Categories = ({ items }: { items: Category[] }): JSX.Element => {
   const { theme } = useTheme();
 
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
   useEffect(() => {
     setIsDarkTheme(theme === "dark" ?? false);
-  }, [ theme]);
+  }, [theme]);
 
+  // TODO: add flex wrap to items on clicking show more
   return (
-    <div className="flex items-center gap-x-4 gap-y-3 pb-2 flex-wrap justify-start">
+    <div className="flex items-center gap-x-4 gap-y-3 justify-start overflow-x-auto pr-6 scroll-container">
+      <div >
+        <Button className="scroll-categories-right">
+          <FaCircleArrowRight />
+        </Button>
+      </div>
       {items.map((item) => (
         <CategoryItem
           key={item?.id}
@@ -33,6 +41,7 @@ const Categories = ({ items }: {items: Category[]}): JSX.Element => {
           isDarkTheme={isDarkTheme}
         />
       ))}
+
     </div>
   );
 }
