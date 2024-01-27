91472e75b5e374e4499aff4012fb378b8c07b494
StphnLwnga <boywilder99@gmail.com>
2024-01-26 16:09:15 -0800

    added course search component to navbar

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -11,6 +11,7 @@ import { cn } from "@/lib/utils";
 import { useState, useEffect } from "react";
 import { Label } from "@/components/ui/label"
 import { Switch } from "@/components/ui/switch"
+import SearchInput from "./search-input";
 
 const NavbarRoutes = (): JSX.Element => {
   const pathname = usePathname();
@@ -33,12 +34,12 @@ const NavbarRoutes = (): JSX.Element => {
 
   const isCreatorPage = pathname?.startsWith("/creator");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search";
 
   useEffect(() => {
     console.log("Instructor mode: ", isInstructorMode);
     if (navFromSwitch)
       router.push(isInstructorMode ? "/creator/courses" : "/");
-    // console.log({ isInstructorMode, navFromSwitch });
     // setNavFromSwitch(false);
   }, [isInstructorMode, navFromSwitch]);
 
@@ -47,56 +48,34 @@ const NavbarRoutes = (): JSX.Element => {
       setIsInstructorMode(true);
       setNavFromSwitch(false);
     }
-    // console.log({ isInstructorMode, navFromSwitch });
   }, [isCreatorPage, isInstructorMode, isPlayerPage, navFromSwitch]);
 
   return (
-    <div className="flex gap-x-6 ml-auto items-center pr-4">
-      {/* {isCreatorPage || isPlayerPage ? (
-        <Link href="/">
-          <Button
-            size="sm"
-            variant="ghost"
-            className={cn(
-              "text-slate-600 hover:text-slate-700",
-              isDarkTheme && "text-slate-200 hover:text-slate-300"
-            )}
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput isDarkTheme={isDarkTheme} />
+        </div>
+      )}
+      <div className="flex gap-x-6 ml-auto items-center pr-4">
+        <div className="flex items-center space-x-2">
+          <Switch
+            checked={isInstructorMode} id="instructor-mode"
+            onCheckedChange={handleInstructorSwitch}
+            className={cn("data-[state=checked]:bg-sky-700", isDarkTheme && "data-[state=checked]:bg-sky-300/30")}
+          />
+          <Label htmlFor="instructor-mode"
+            className={cn("text-slate-600 hover:text-slate-700", isDarkTheme && "text-slate-200 hover:text-slate-300")}
           >
-            <LogOut className="h-4 w-4 mr-2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/creator/courses">
-          <Button
-            size="sm"
-            variant="ghost"
-            className={cn(
-              "text-slate-600 hover:text-slate-700",
-              isDarkTheme && "text-slate-200 hover:text-slate-300"
-            )}
-          >
-            Creator Mode
-          </Button>
-        </Link>
-      )} */}
-      <div className="flex items-center space-x-2">
-        <Switch
-          checked={isInstructorMode} id="instructor-mode"
-          onCheckedChange={handleInstructorSwitch}
-          className={cn("data-[state=checked]:bg-sky-700", isDarkTheme && "data-[state=checked]:bg-sky-300/40")}
-        />
-        <Label htmlFor="instructor-mode"
-          className={cn("text-slate-600 hover:text-slate-700", isDarkTheme && "text-slate-200 hover:text-slate-300")}
-        >
-          Instructor Mode
-        </Label>
+            Instructor Mode
+          </Label>
+        </div>
+        <ModeToggle />
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
       </div>
-      <ModeToggle />
-      <SignedIn>
-        <UserButton afterSignOutUrl="/sign-in" />
-      </SignedIn>
-    </div>
+    </>
   );
 }
 
---
+++ components/search-input.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { Search } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+
+
+interface SearchInputProps {
+  isDarkTheme: boolean;
+}
+
+const SearchInput = ({ isDarkTheme }: SearchInputProps): JSX.Element => {
+  return (
+    <div className="relative">
+      <Search
+        className={cn(
+          "h-4 w-4 absolute top-3 left-3",
+        )}
+      />
+      <Input
+        className={cn(
+          "w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200",
+          isDarkTheme && "bg-sky-300/30"
+        )}
+        placeholder="Search course title..."
+      />
+    </div>
+  );
+}
+
+export default SearchInput;
