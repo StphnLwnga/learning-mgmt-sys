2973307571a2cfa1d961d8312080b537c06a1fd9
StphnLwnga <boywilder99@gmail.com>
2024-01-29 14:13:54 -0800

    added course mobile sidebar component

---
+++ app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
@@ -0,0 +1,20 @@
+import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { CourseNavbarProps } from "./course-navbar";
+import CourseSidebar from "./course-sidebar";
+
+const CourseMobileSidebar = ({ course, progressCount }: CourseNavbarProps): JSX.Element => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition z-60">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-white w-72">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default CourseMobileSidebar;
