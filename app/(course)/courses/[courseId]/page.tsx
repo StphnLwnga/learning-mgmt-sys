99ffb28d05d515d86a4ef3cf0258c90c473f0c2d
StphnLwnga <boywilder99@gmail.com>
2024-02-03 14:43:29 -0800

    added courseId page

--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,9 +1,22 @@
-const CourseIdPage = () => {
-  return (
-    <div className="p-6">
-      Course Page
-    </div>
-  );
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string; } }): Promise<JSX.Element> => {
+  const { courseId } = params;
+
+  const course = await db.course.findUnique({
+    where: { id: courseId },
+    include: {
+      chapters: {
+        where: { isPublished: true },
+        orderBy: { position: 'asc', },
+      },
+    },
+  });
+
+  if (!course) return redirect("/");
+
+  return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`);
 }
- 
+
 export default CourseIdPage;
