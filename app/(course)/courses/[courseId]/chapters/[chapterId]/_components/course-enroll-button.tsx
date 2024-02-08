3af1c5d6212c0628179404e52e882cbe0854cb51
StphnLwnga <boywilder99@gmail.com>
2024-02-08 05:17:34 -0800

    enabled course enrollment button

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,7 +1,13 @@
 "use client"
 
+import axios from "axios";
+import { useState } from "react";
+
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+
 
 interface CourseEnrollButtonProps {
   courseId: string;
@@ -9,8 +15,34 @@ interface CourseEnrollButtonProps {
 }
 
 const CourseEnrollButton = ({ courseId, price }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { toast } = useToast();
+
+  const onClick = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+
+      window.location.assign(response.data.url);
+    } catch (error) {
+      console.log("[COURSE_ENROLLMENR]", error);
+      toast({
+        title: 'Error',
+        description: "Something went wrong!",
+        variant: 'destructive',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
-    <Button size="sm" className="w-full md:w-auto">
+    <Button
+      size="sm"
+      disabled={isLoading}
+      onClick={onClick}
+      className="w-full md:w-auto"
+    >
       Enroll for {formatPrice(price)}
     </Button>
   );
