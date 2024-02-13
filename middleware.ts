bcd212d39ebba1ddfb9bdfefe588fbd28e111304
StphnLwnga <boywilder99@gmail.com>
2024-02-13 15:31:11 -0800

    i18n - changed lang middleware & added translations

--- middleware.ts
+++ middleware.ts
@@ -2,5 +2,16 @@ import { chainMiddlewares } from "./middlewares/chain-middlewares";
 import { withAuth, withLang } from "./middlewares";
 
 export default chainMiddlewares([
-  withAuth, withLang,
+  withAuth, 
+  withLang,
 ]);
+
+export const config = {
+  // Matcher ignoring `/_next/` and `/api/`
+  matcher: [
+    '/((?!.+\\.[\\w]+$|_next).*)', 
+    '/', 
+    '/(api|trpc)(.*)', 
+    '/((?!api|_next/static|_next/image|favicon.ico).*)'
+  ]
+}
