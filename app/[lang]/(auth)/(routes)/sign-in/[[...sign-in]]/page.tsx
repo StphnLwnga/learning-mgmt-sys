9b6e0af50d566a3fe1a7067ef4d310499e609479
StphnLwnga <boywilder99@gmail.com>
2024-02-14 11:44:55 -0800

    redirect on sign-up/sign-in

--- app/[lang]/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/[lang]/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,13 @@
+import { Locale } from "@/i18n";
 import { SignIn } from "@clerk/nextjs";
- 
+
 /**
  * Renders the Page component.
  *
  * @return {JSX.Element} The rendered SignIn component.
  */
-export default function Page(): JSX.Element {
-  return <SignIn />;
+export default function Page({ params }: { params: { lang: Locale } }): JSX.Element {
+  const { lang } = params;
+
+  return <SignIn redirectUrl={`${process.env.NEXT_PUBLIC_APP_URL}/${lang}`} />;
 }
