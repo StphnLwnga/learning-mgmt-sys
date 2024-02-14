d5eaa35087b5a21a9bec3f484b33c3d824d10b74
StphnLwnga <boywilder99@gmail.com>
2024-02-14 10:59:54 -0800

    navbar - fixed localization

--- app/[lang]/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
+++ app/[lang]/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
@@ -4,14 +4,19 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { CourseNavbarProps } from "./course-navbar";
 import CourseSidebar from "./course-sidebar";
 
-const CourseMobileSidebar = ({ course, progressCount }: CourseNavbarProps): JSX.Element => {
+const CourseMobileSidebar = ({ course, progressCount, t, lang }: CourseNavbarProps): JSX.Element => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition z-60">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-white w-72">
-        <CourseSidebar course={course} progressCount={progressCount} />
+        <CourseSidebar
+          t={t}
+          lang={lang}
+          course={course}
+          progressCount={progressCount}
+        />
       </SheetContent>
     </Sheet>
   );
