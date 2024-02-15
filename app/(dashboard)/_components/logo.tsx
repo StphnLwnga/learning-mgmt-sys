473970b8e18dcd6e920aac9086d5dcf3f0319eb9
StphnLwnga <boywilder99@gmail.com>
2024-02-14 17:40:31 -0800

    replaced logo

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useTheme } from "next-themes";
 
-
-const Logo = ({isDarkTheme}: {isDarkTheme: boolean}): JSX.Element => {
+const Logo = ({ isDarkTheme, lang }: { isDarkTheme: boolean, lang?: string }): JSX.Element => {
   return (
-    <div className="flex justify-center items-center">
-      <Image 
-        height={180}
-        width={200}
-        alt="logo"
-        src={isDarkTheme ? '/CourseNoma-logos_white.png' : '/CourseNoma-logos_black.png'}
-        priority
-      />
-    </div>
+    <Link href={lang ? `/${lang}` : "/"}>
+      <div className="flex justify-center items-center">
+        <Image
+          height={180}
+          width={180}
+          alt="logo"
+          priority
+          src={isDarkTheme ? '/coursenoma_book_white.png' : '/coursenoma_book_blu.png'}
+        />
+      </div>
+    </Link >
   );
 }
- 
+
 export default Logo;
