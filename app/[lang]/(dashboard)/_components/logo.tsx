15750c59ad4533f830676b2b8c3cd5657459c0ba
StphnLwnga <boywilder99@gmail.com>
2024-02-16 16:43:17 -0800

    logo - bug fixed

--- app/[lang]/(dashboard)/_components/logo.tsx
+++ app/[lang]/(dashboard)/_components/logo.tsx
@@ -17,22 +17,3 @@ const Logo = ({ isDarkTheme, lang }: { isDarkTheme: boolean, lang?: string }): J
     </Link >
   );
 }
-
-export default Logo;import Image from "next/image";
-
-
-const Logo = ({isDarkTheme}: {isDarkTheme: boolean}): JSX.Element => {
-  return (
-    <div className="flex justify-center items-center">
-      <Image 
-        height={180}
-        width={200}
-        alt="logo"
-        src={isDarkTheme ? '/CourseNoma-logos_white.png' : '/CourseNoma-logos_black.png'}
-        priority
-      />
-    </div>
-  );
-}
- 
-export default Logo;
